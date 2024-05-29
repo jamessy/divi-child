547de9ecc0f486406a9c6c4799e8e66bb1a83ca8
jamessy <jamesgodwin2@googlemail.com>
2024-05-29 13:38:54 +0100

    Update webpack.mix.js
    updated mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,4 +5,4 @@ let mix = require('laravel-mix');
 mix.js('src/js/scripts.js', 'js')
 .sass('src/styles/scss/styles.scss', 'css')
 .setPublicPath('dist')
-.browserSync('http://floorgallery.test');
+.browserSync('http://tobeupdated');

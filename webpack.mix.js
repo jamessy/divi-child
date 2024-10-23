2964e9a57ba11a702f4cea60e4b6bafc7a91eaf7
jamessy <jamesgodwin2@googlemail.com>
2024-10-23 13:25:41 +0100

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,5 +5,6 @@ let mix = require("laravel-mix");
 mix
   .js("src/js/scripts.js", "js")
   .sass("src/styles/scss/styles.scss", "css")
+  .version()
   .setPublicPath("dist")
   .browserSync("https://ukbusinessai.test");
